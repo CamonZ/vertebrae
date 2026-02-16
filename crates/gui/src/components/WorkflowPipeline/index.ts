72fa02640b418c7536c504bfae751abab5932545
Simon Garcia <camonz@camonz.com>
2026-02-16 22:02:45 +0100

    [2b250a66] Clean up removed TaskZoneNode files and exports
    Delete TaskZoneNode.tsx and TaskZoneNode.test.tsx, remove the
    TaskZoneNode export from WorkflowPipeline/index.ts.

--- crates/gui/src/components/WorkflowPipeline/index.ts
+++ crates/gui/src/components/WorkflowPipeline/index.ts
@@ -1,6 +1,5 @@
 export { StepNode, type StepNodeData, type StepNodeType } from './StepNode';
 export { WorkflowZoneNode, type WorkflowZoneNodeData, type WorkflowZoneNodeType, COLLAPSED_WORKFLOW_WIDTH, COLLAPSED_WORKFLOW_HEIGHT } from './WorkflowZoneNode';
-export { TaskZoneNode, type TaskZoneNodeData, type TaskZoneNodeType } from './TaskZoneNode';
 export { ElkRoutedEdge, type ElkRoutedEdgeData, type ElkRoutedEdgeType } from './ElkRoutedEdge';
 export { getStatusColor, getStatusIcon, getLevelDotColor } from './taskUtils';
 export {
